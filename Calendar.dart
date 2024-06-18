1ef58a427a6a7253d500fd93201578ac93e95439
Maxim <123577115+Max1m3L@users.noreply.github.com>
2024-06-18 17:54:17 +0300

    Add files via upload

---
+++ Calendar.dart
@@ -0,0 +1,73 @@
+// calendar.dart
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Календарь'),
+      ),
+      body: TableCalendar(
+        initialCalendarFormat: CalendarFormat.month,
+        calendarStyle: CalendarStyle(
+            todayColor: Colors.orange,
+            selectedColor: Theme.of(context).primaryColor,
+            todayStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.white)),
+        headerStyle: HeaderStyle(
+          centerHeaderTitle: true,
+          formatButtonDecoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          formatButtonTextStyle: TextStyle(color: Colors.white),
+          formatButtonShowsNext: false,
+        ),
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        onDaySelected: (date, events) {
+          print(date.toIso8601String());
+        },
+        builders: CalendarBuilders(
+          selectedDayBuilder: (context, date, events) => Container(
+              margin: const EdgeInsets.all(4.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Text(
+                date.day.toString(),
+                style: TextStyle(color: Colors.white),
+              )),
+          todayDayBuilder: (context, date, events) => Container(
+              margin: const EdgeInsets.all(4.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Text(
+                date.day.toString(),
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+        calendarController: _controller,
+      ),
+    );
+  }
+}
